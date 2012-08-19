d59a58a765082eb647f7e2853fd12a7bc6ca1e9c
Extazystas <z1m@tut.by>
2012-08-19 13:27:54 +0300

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microdemo::Application.config.secret_token = '890b002d8ed4c59913b0a605134d388be178027311a4e298c963f69ddee996ec4d1d7c65982f5547575d8c0c040a76b63056ea0cfff336edfbf6e81df6eb5d28'
